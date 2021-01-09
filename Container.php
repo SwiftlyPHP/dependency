5796514213386cd0f66d48816aeaa2be0a7bec6f
clvarley <conorlukevarley@gmail.com>
2021-01-09 20:14:43 +0000

    Added methods to service container

--- Container.php
+++ Container.php
@@ -26,16 +26,50 @@ Class Container
      * @param LoaderInterface $loader Dependency loader
      * @return self                   Allow chaining
      */
-    public function load() : Container
+    public function load( LoaderInterface $loader ) : Container
     {
         return $loader->load( $this );
     }
 
     /**
-     * 
+     * Binds a new service by name
+     *
+     * @param string $name   Service name
+     * @param mixed $service Service provider
+     * @return Service       Allow chaining
      */
     public function bind( string $name, /* object */ $service ) : Service
     {
-        // TODO:
+        $this->services[$name] = new Service( $service );
+
+        return $this->services[$name];
+    }
+
+    /**
+     * Used to set an alias for a service
+     *
+     * Please do not rely on this method, it may be reworked/removed in a
+     * future release. Instead use the {@see Service::alias} method.
+     *
+     * @internal
+     * @param string $name     Service name
+     * @param Service $service Service definition
+     * @return Service         Allow chaining
+     */
+    public function alias( string $name, Service $service ) : Service
+    {
+        $this->services[$name] = $service;
+
+        return $this->services[$name];
+    }
+
+    /**
+     * Checks to see if a named service is available
+     *
+     * @return bool Service available?
+     */
+    public function has() : bool
+    {
+        return isset( $this->services[$name] );
     }
 }
--- Loader/PhpLoader.php
+++ Loader/PhpLoader.php
@@ -8,21 +8,84 @@ use Swiftly\Dependency\{
 };
 
 /**
+ * Class responsible for loading services from PHP files
  *
+ * @author clvarley
  */
 Class PhpLoader Implements LoaderInterface
 {
 
     /**
-     * Attempts to load the dependencies from PHP file
+     * Path to the PHP services file
+     *
+     * @var string $file File path
+     */
+    protected $file;
+
+    /**
+     * Create loader for the given PHP service file
+     *
+     * @param string $file File path
+     */
+    public function __construct( string $file )
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Load services into this dependency container
      *
      * @param Container $container Dependency container
      * @return void                N/a
      */
     public function load( Container $container ) : Container
     {
-        // TODO: Load stuff!
+        if ( !$this->valid() ) {
+            return $container;
+        }
+
+        // TODO: Sandbox this in future!
+        $services = include ($this->file);
+
+        if ( empty( $services ) || !\is_array( $services ) ) {
+            return $container;
+        }
+
+        // Parse the service structures
+        foreach ( $services as $name => $service ) {
+            if ( !\is_string( $name ) || empty( $service ) ) {
+                continue;
+            }
+
+            // Callback func or service name?
+            if ( \is_string( $service ) ) {
+                $handler = $service;
+            } elseif ( !empty( $service['handler'] ) && \is_callable( $service['handler'] ) ) {
+                $handler = $service['handler'];
+            } else {
+                $handler = $name;
+            }
+
+            $registered = $container->bind( $name, $handler );
+
+            // Explicitly not a singleton?
+            if ( isset( $service['singleton'] ) && empty( $service['singleton'] ) ) {
+                $registered->singleton( false );
+            }
+        }
 
         return $container;
+    }
+
+    /**
+     * Checks to see if the file is valid
+     *
+     * @return bool File valid
+     */
+    private function valid() : bool
+    {
+        return ( \is_readable( $this->file )
+            && \substr( $this->file, -4 ) === '.php'
+        );
     }
 }
