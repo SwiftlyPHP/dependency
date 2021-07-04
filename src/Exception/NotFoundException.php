418e60980727f299864d68b2b016c2b4b0bb9219
clvarley <conorlukevarley@gmail.com>
2021-07-04 01:48:52 +0100

    Added exceptions

---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Swiftly\Dependency\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown if a class cannot be found in the service container
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Final Class NotFoundException Implements RuntimeException
+{
+    // TODO:
+}
---
+++ src/Exception/ResolutionException.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace Swiftly\Dependency\Exception;
