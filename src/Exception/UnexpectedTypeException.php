40b0eb9bcb97e73c236e33dc9b7bcb633de73ad0
clvarley <conorlukevarley@gmail.com>
2021-10-12 19:55:08 +0100

    Ability to return result, fixed names for non-objects

--- src/Exception/UnexpectedTypeException.php
+++ src/Exception/UnexpectedTypeException.php
@@ -5,9 +5,14 @@ namespace Swiftly\Dependency\Exception;
 use UnexpectedValueException;
 
 use function sprintf;
+use function is_object;
+use function get_class;
+use function gettype;
 
 /**
  * Exception thrown if a resolved service is not of the expected type
+ *
+ * @template TActual
  *
  * @author clvarley
  */
@@ -15,19 +20,39 @@ Final Class UnexpectedTypeException Extends UnexpectedValueException
 {
 
     /**
+     * @psalm-var TActual
+     *
+     * @var mixed $actual
+     */
+    private $actual;
+
+    /**
      * @psalm-param class-string $expected
+     * @psalm-param TActual $actual
      *
      * @param string $expected Expected class
-     * @param object $actual   Actual result
+     * @param mixed $actual    Actual result
      */
     public function __construct( string $expected, $actual )
     {
+        $this->actual;
+
         parent::__construct(
             sprintf(
-                'Container expected to resolve a dependency of type %s, but instead resolved a %s',
+                'Container expected to resolve a dependency of type (%s), but instead resolved (%s)',
                 $expected,
-                get_class( $actual )
+                is_object( $actual ) ? get_class( $actual ) : gettype( $actual )
             )
         );
+    }
+
+    /**
+     * @psalm-return TActual
+     *
+     * @return mixed Actual result
+     */
+    public function getActual() // : mixed
+    {
+        return $this->actual;
     }
 }
